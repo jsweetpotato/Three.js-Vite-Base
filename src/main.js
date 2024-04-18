cee249b74c4427a19023739da69480cee5753c7d
jsweetpotato37 <jsweetpotato37@gmail.com>
2024-04-18 19:22:17 +0900

    class 타입 vite THREE.js Base 템플릿

--- src/main.js
+++ src/main.js
@@ -5,82 +5,100 @@ import { GUI } from "lil-gui";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
 
-THREE.ColorManagement.enabled = false;
-
-const canvas = document.querySelector(".webgl-canvas");
-
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
+class App {
+  constructor() {
+    THREE.ColorManagement.enabled = false;
 
-/* ------- Util Base ------- */
-const gui = new GUI();
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 100);
-const controls = new OrbitControls(camera, canvas);
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    const canvas = document.querySelector(".webgl-canvas");
+    this._canvas = canvas;
 
-/* --------------------- */
-/* --- Util Settings --- */
-renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
-// scene.background = new THREE.Color("#2d2d2d");
-controls.enableDamping = true;
+    // Renderer settings
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+    this._renderer = renderer;
 
-// camera.position.y = 1;
-camera.position.z = 5;
+    const scene = new THREE.Scene();
+    this._scene = scene;
 
-/* ------------------------ */
-/* -------- Events -------- */
+    const clock = new THREE.Clock();
+    this._clock = clock;
 
-const handleResize = () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+    this._setCamera();
+    this._setLight();
+    this._setModel();
+    this._setControls();
+    this._setGUI();
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    window.onresize = this.resize.bind(this);
+    this.resize();
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  _setCamera() {
+    const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 100);
+    camera.position.z = 5;
+    this._camera = camera;
+  }
+
+  _setLight() {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+    this._scene.add(ambientLight);
+  }
+
+  _setModel() {
+    const mat = new THREE.ShaderMaterial({
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+    });
+    const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), mat);
+
+    this._scene.add(plane);
+  }
+
+  _setControls() {
+    const controls = new OrbitControls(this._camera, this._canvas);
+    controls.enableDamping = true;
+    this._controls = controls;
+  }
+
+  _setGUI() {
+    const gui = new GUI();
+  }
+
+  render() {
+    const elapsedTime = this._clock.getElapsedTime();
+    // material.uniforms.uTime.valaue = elapsedTime;
+
+    // Update controls
+    this._controls.update();
+
+    // Render
+    this._renderer.render(this._scene, this._camera);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  resize() {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    this._camera.aspect = sizes.width / sizes.height;
+    this._camera.updateProjectionMatrix();
+
+    // Update renderer
+    this._renderer.setSize(sizes.width, sizes.height);
+    this._renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  }
+}
+
+window.onload = function () {
+  new App();
 };
-
-window.addEventListener("resize", handleResize);
-
-/* ----------------------- */
-/* ------- Objects ------- */
-
-const mat = new THREE.ShaderMaterial({
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader,
-});
-
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), mat);
-
-scene.add(plane);
-
-/* ----------------------- */
-/* ------- Animate ------- */
-
-const clock = new THREE.Clock();
-
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  // material.uniforms.uTime.value = elapsedTime;
-
-  // Update controls
-  controls.update();
-
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-
-tick();
